ef5b0285c839cee8ea1b47c58dbe08d000d8df7b
José Támara <jochitoel1@hotmail.com>
2018-08-28 03:15:01 -0500

    NavVertical

--- src/Component/NavVertical.js
+++ src/Component/NavVertical.js
@@ -4,11 +4,19 @@ import { Nav, NavItem, NavLink } from 'reactstrap';
 function NavVertical(){
 	return(
 		<div className="NavVertical">
-			<p>HOME</p>
+			<NavLink href="" className="cel-border title"> <span className="t">Jk</span>zasori</NavLink>
+			<div className="content-title">
+				<NavLink href="" className="Dash">
+					<span className="fa fa-space-shuttle icon"> </span> Dashboard
+				</NavLink>
+				<h3> UI Content </h3>
+			</div>
 			<Nav vertical>
 				<NavItem>
-					<NavLink href="">Category</NavLink>
-					<NavLink href="#">Dos</NavLink>
+					<div className="links"><NavLink href="#" className="cel-border "><span className="fa fa-wrench icon"> </span>Tools</NavLink></div>
+					<div className="links"><NavLink href="" className="cel-border "><span className="fa fa-list-ul icon"> </span>Categories</NavLink></div>
+					<div className="links"><NavLink href="#" className="cel-border "><span className="fa fa-user icon"> </span>Users</NavLink></div>
+					<div className="links"><NavLink href="#" className="cel-border "><span className="fa fa-image icon"> </span>Results</NavLink></div>
 				</NavItem>
 			</Nav>
 		</div>
